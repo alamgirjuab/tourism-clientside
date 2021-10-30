b4f5a0820bda195da9d84ec5379f710123cbc539
alamgirjuab <alamgirjuab@gmail.com>
2021-10-30 20:41:34 +0600

    banner updated

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
                     <div className='row align-items-center'>
                         <div className='col-lg-6 col-md-6 col-sm-12 text-white'>
                             <h1>Amazing Tour in Sundarban</h1>
-                            <p>Discover youself in one of the World Heritage sites of Bangladesh.</p>
+                            <p className='my-4'>Discover youself in one of the World Heritage sites of Bangladesh.</p>
                             <button className='btn btn-danger'>Book Now</button>
                         </div>
                         <div className='col-lg-6 col-md-6 col-sm-12 my-5'>
